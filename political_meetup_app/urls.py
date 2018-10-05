34a966bbb125343215f6682ef06c48669208a8ef
yinebeb Girma <yinebebgirma@yinebebs-MacBook-Pro.local>
2018-10-05 10:25:32 -0700

    add user to db

--- political_meetup_app/urls.py
+++ political_meetup_app/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
-    path('profile/', views.profile, name='profile'),
-    path('create_profile', views.profile, name='create_profile'),
+    path('profile/', views.create_profile, name='profile'),
+    path('create_profile/', views.create_profile, name='create_profile'),
     path('home/', views.home, name='home'),
     path('map', views.VenueAdmin.as_view(), name='map')
 ]
